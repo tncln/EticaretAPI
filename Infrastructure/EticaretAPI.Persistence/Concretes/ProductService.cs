339f4c3c647c1a3da590363ab568e64718ffcae3
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-02-26 00:54:16 +0300

    Onion Architecture inşa

---
+++ Infrastructure/EticaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using EticaretAPI.Application.Abstractions;
+using EticaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EticaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new()
+        {
+            new(){ Id = Guid.NewGuid(), CreatedDate = DateTime.Now , Name = "Product 1 ", Price= 100, Stock=10},
+            new(){ Id = Guid.NewGuid(), CreatedDate = DateTime.Now , Name = "Product 2 ", Price= 200, Stock=10},
+            new(){ Id = Guid.NewGuid(), CreatedDate = DateTime.Now , Name = "Product 3 ", Price= 300, Stock=30},
+            new(){ Id = Guid.NewGuid(), CreatedDate = DateTime.Now , Name = "Product 4 ", Price= 400, Stock=10},
+            new(){ Id = Guid.NewGuid(), CreatedDate = DateTime.Now , Name = "Product 5 ", Price= 500, Stock=50},
+        };
+    }
+}
---
+++ Infrastructure/EticaretAPI.Persistence/ServiceRegisteration.cs
@@ -0,0 +1,20 @@
+﻿using EticaretAPI.Application.Abstractions;
+using EticaretAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EticaretAPI.Persistence
+{
+    public static class ServiceRegisteration
+    {
+        //API da yer alan IOC eklemek için kullanılır. 
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
